cfe56210da1c6de7cb9b05aa737f7431a67491e4
Nick Capurso <capurso.nick@gmail.com>
2015-03-02 17:31:45 -0500

    Added AsyncTask to IncidentActivity

--- app/src/main/java/com/example/nickcapurso/mapsapplication/IncidentsActivity.java
+++ app/src/main/java/com/example/nickcapurso/mapsapplication/IncidentsActivity.java
@@ -1,6 +1,8 @@
 package com.example.nickcapurso.mapsapplication;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 /**
@@ -12,4 +14,37 @@ public class IncidentsActivity extends Activity {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_incidents);
    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        ProgressDialog.show(this, "Please Wait...", "Loading Incidents...", true);
+    }
+
+    private class FetchIncidents extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            for(int i=0;i<5;i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
 }
