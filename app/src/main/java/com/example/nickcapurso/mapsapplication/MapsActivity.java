193ede8599388c23c7630f904b470a31248589df
Nick Capurso <capurso.nick@gmail.com>
2015-03-01 19:36:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nickcapurso/mapsapplication/MapsActivity.java
@@ -0,0 +1,114 @@
+package com.example.nickcapurso.mapsapplication;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+
+        new WmataTest().execute("n/a");
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    class WmataTest extends AsyncTask<String, Void, Integer> {
+
+        protected Integer doInBackground(String... urls) {
+            HttpClient httpclient = (HttpClient) HttpClients.createDefault();
+
+            try{
+                URIBuilder builder = new URIBuilder("https://api.wmata.com/Rail.svc/json/jLines");
+                // Specify your subscription key
+                Log.d("TEST", "1");
+                builder.setParameter("api_key", "kfgpmgvfgacx98de9q3xazww");
+                URI uri = builder.build();
+                HttpGet request = new HttpGet(uri);
+                Log.d("TEST", "2");
+
+                HttpResponse response = httpclient.execute(request);
+                Log.d("TEST", "3");
+                HttpEntity entity = response.getEntity();
+                Log.d("TEST", "4");
+                if (entity != null) {
+                    System.out.println(EntityUtils.toString(entity));
+                }
+                return 1;
+            }
+            catch (IOException e)
+            {
+                Log.d("TEST", "IOException");
+            } catch (URISyntaxException e) {
+                Log.d("TEST", "URISyntaxException");
+            }
+            return 0;
+        }
+    }
+}
